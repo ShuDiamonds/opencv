a7e3d20c0b5c465c8bb4df258e17306ef5a55b0c
ShuDiamonds <fukuda0729android@gmail.com>
2019-03-20 13:43:10 +0900

    first commit

---
+++ segmantation/hsv.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 10:57:27 2019
+
+@author: shuichi
+
+opencvのがそのあつかい：http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_core/py_basic_ops/py_basic_ops.html
+"""
+
+import cv2
+import numpy as np
+import copy
+
+def main():
+    # 入力画像の読み込み
+    img = cv2.imread("./lightdesign/myroom2.jpg")
+
+    # 方法2(OpenCVで実装)       
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    #hsv分割
+    Hchanel,Schanel,Vchanel=cv2.split(hsv)
+    hsv_transed=copy.deepcopy(hsv)
+    
+    hsv_transed[:,:,1] = 255
+    hsv_transed[:,:,2] = 255#int(np.mean(Vchanel))
+    # 結果を出力
+    #cv2.imwrite("hsv.jpg", hsv)
+    cv2.imshow('hsv_transed',cv2.cvtColor(hsv_transed, cv2.COLOR_HSV2BGR))
+
+    cv2.imshow('image',img)
+    #hsv output
+    
+    cv2.imshow('h',Hchanel)
+    cv2.imshow('s',Schanel)
+    cv2.imshow('v',Vchanel)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__":
+    main()
+
+    # hsv変換
+    """
+    green = np.uint8([[[0,255,0 ]]])
+    hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
+    print(hsv_green)
+    """
---
+++ segmantation/segmentation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 10:57:27 2019
+
+@author: shuichi
+
+パラメータ 	
+sigma 	境界線の滑らかさ（複雑な境界線には小さい値、滑らかな境界線には大きい値）
+k 	たぶん、どれくらい候補領域を統合するか（値が小さいと多くの小さい領域、大きいと少ない大きな領域に分割？）
+min_size 	領域の最小サイズ（たぶん領域のピクセル数）
+
+
+opencvのがそのあつかい：http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_core/py_basic_ops/py_basic_ops.html
+"""
+
+import cv2
+import numpy as np
+
+segmentator = cv2.ximgproc.segmentation.createGraphSegmentation(sigma=0.5, k=300, min_size=1000)
+src = cv2.imread('./lightdesign/03.jpg')
+segment = segmentator.processImage(src)
+
+mask = segment.reshape(list(segment.shape) + [1]).repeat(3, axis=2)
+masked = np.ma.masked_array(src, fill_value=0)
+for i in range(np.max(segment)):
+    masked.mask = mask != i
+    y, x = np.where(segment == i)
+    top, bottom, left, right = min(y), max(y), min(x), max(x)
+    dst = masked.filled()[top : bottom + 1, left : right + 1]
+    cv2.imwrite('./segment_pic/segment_{num}.jpg'.format(num=i), dst)
+
+
+segment = segment*(int(255/segment.max())-1)
+cv2.imwrite('./segment_pic/segment_all.jpg', segment)
